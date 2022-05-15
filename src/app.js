298092158442bb8a9d10ea8d77de601e172f1e64
Jarek <jarek@booste.com>
2022-05-15 18:48:34 +0200

    Template changes to display nicer on Heroku deploy

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs');
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
+const { author } = require('./../package.json');
+
 const appPublicPath = path.join(__dirname, '..', 'public');
 const appViewsPath = path.join(__dirname, '..', 'templates', 'views');
 const appPartialsPath = path.join(__dirname, '..', 'templates', 'partials');
@@ -19,19 +21,19 @@ hbs.registerPartials(appPartialsPath)
 app.get('/', (req, res, next) => {
     res.render('index.hbs', {
         title: 'Weather Hbs',
-        name: process.env.npm_package_author_name,
+        author,
     });
 });
 app.get('/about', (req, res, next) => {
     res.render('about', {
         title: 'About Hbs',
-        name: process.env.npm_package_author_name,
+        author,
     });
 });
 app.get('/help', (req, res, next) => {
     res.render('help', {
         title: 'Help Hbs',
-        name: process.env.npm_package_author_name,
+        author,
     });
 });
 
@@ -43,9 +45,6 @@ app.get('/weather', (req, res, next) => {
     }
 
     const address = decodeURIComponent(req.query.address);
-    //address = 'Przecław 52, 72-005 Przecław, województwo zachodniopomorskie, Polska';
-    //address = 'Barnówko 50, 74-311 Barnówko, województwo zachodniopomorskie, Polska';
-    //address = 'Płoty, 72-310 Płoty, województwo zachodniopomorskie, Polska';
     geocode(address, (error, {latitude, longitude, location} = {}) => {
         if (error) {
             return res.send({error});
@@ -66,30 +65,21 @@ app.get('/weather', (req, res, next) => {
             });
         });
     });
-
-
 });
-
-// app.get('/products', (req, res, next) => {
-//     console.log(req.query.param1);
-//     res.send({
-//         products: []
-//     });
-// });
 
 app.get('/help/*', (req, res, next) => {
     res.render('errors/404', {
         title: 'Help 404 page',
-        name: process.env.npm_package_author_name,
-        message: 'Article not found'
+        message: 'Article not found',
+        author,
     });
 });
 
 app.get('/*', (req, res, next) => {
     res.render('errors/404', {
         title: 'Error 404 page',
-        name: process.env.npm_package_author_name,
-        message: 'Page not found'
+        message: 'Page not found',
+        author,
     });
 });
 
